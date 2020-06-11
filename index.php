074a8e516cfc9fa472074a5ea6238a89fe7a8a67
Tien Nguyen <tien.nguyen@s3corp.com.vn>
2020-06-11 14:06:41 +0700

    add mobile events.

--- index.php
+++ index.php
@@ -53,6 +53,7 @@
   <script src="js/jquery-3.5.1.min.js"></script>
   <script src="js/popper.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
+  <script src="js/hammer.js"></script>
   <script src="load.js"></script>
 </body>
 
--- load.js
+++ load.js
@@ -6,13 +6,70 @@ jQuery.expr[':'].contains = function(a, i, m) {
 var polyline = null;
 var panZoomTiger = null;
 $(function () {
+  var eventsHandler = {
+    haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel']
+  , init: function(options) {
+      var instance = options.instance
+        , initialScale = 1
+        , pannedX = 0
+        , pannedY = 0
+
+      // Init Hammer
+      // Listen only for pointer and touch events
+      this.hammer = Hammer(options.svgElement, {
+        inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
+      })
+
+      // Enable pinch
+      this.hammer.get('pinch').set({enable: true})
+
+      // Handle double tap
+      this.hammer.on('doubletap', function(ev){
+        instance.zoomIn()
+      })
+
+      // Handle pan
+      this.hammer.on('panstart panmove', function(ev){
+        // On pan start reset panned variables
+        if (ev.type === 'panstart') {
+          pannedX = 0
+          pannedY = 0
+        }
+
+        // Pan only the difference
+        instance.panBy({x: ev.deltaX - pannedX, y: ev.deltaY - pannedY})
+        pannedX = ev.deltaX
+        pannedY = ev.deltaY
+      })
+
+      // Handle pinch
+      this.hammer.on('pinchstart pinchmove', function(ev){
+        // On pinch start remember initial zoom
+        if (ev.type === 'pinchstart') {
+          initialScale = instance.getZoom()
+          instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x, y: ev.center.y})
+        }
+
+        instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x, y: ev.center.y})
+      })
+
+      // Prevent moving the page on some devices when panning over SVG
+      options.svgElement.addEventListener('touchmove', function(e){ e.preventDefault(); });
+    }
+
+  , destroy: function(){
+      this.hammer.destroy()
+    }
+  }
+
   panZoomTiger = svgPanZoom('#svg-container svg', {
     zoomEnabled: true,
     controlIconsEnabled: true,
     fit: true,
     center: true,
     minZoom: 0.5,
-    contain: false
+    contain: false,
+    customEventsHandler: eventsHandler
   });
 
   $('#btnSearch').click(function(event) {
@@ -22,7 +79,7 @@ $(function () {
     panZoomTiger.reset();
 
     if (polyline) {
-      polyline.removeClass('selected');
+      polyline.removeClass('plot-selected');
       polyline = null;
     }
 
@@ -36,7 +93,7 @@ $(function () {
       return false;
     }
 
-    polyline.addClass('selected');
+    polyline.addClass('plot-selected');
 
     var point = polyline.attr('points').split(" ", 1)[0].split(",");
     var a = Math.round(point[0]) + 50;
